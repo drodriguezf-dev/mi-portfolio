e05b9acaa93007ea6e4d9e9f92fa97ae15f43fdc
David Ferrete <drodriguezfdev@gmail.com>
2025-10-30 20:06:44 +0100

    No

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -77,7 +77,7 @@ export default function Hero() {
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full size-2 bg-green-500"></span>
             </span>
-            Disponible para proyectos 2
+            Disponible para proyectos
           </span>
         </motion.div>
 
