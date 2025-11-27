485c2eabac8241a3f4e60f75eab2ffae7f012cca
David Ferrete <drodriguezfdev@gmail.com>
2025-11-27 12:22:08 +0100

    FotoArreglada

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -63,7 +63,7 @@ export default function Hero() {
             />
             {/* Photo */}
             <Image
-              src="https://archivos.dferretedev.com/images/fotoPortfolio.webp"
+              src="https://archivos.dferretedev.com/fotoPortfolio.webp"
               alt="David Rodríguez Ferrete"
               width={128}
               height={128}
